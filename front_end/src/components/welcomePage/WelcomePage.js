e5bb71f46a18d072e7b27a6e9e17a00469fb03d2
PsijicSeht <38445096+Kumar-Krishnan@users.noreply.github.com>
2020-04-09 22:04:43 -0400

    Added semantic ui to style welcome page

--- front_end/src/components/welcomePage/WelcomePage.js
+++ front_end/src/components/welcomePage/WelcomePage.js
@@ -1,19 +1,48 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Button, Checkbox, Form } from 'semantic-ui-react'
 
 export default class WelcomePage extends Component {
+  state = {
+    userName: "",
+    password: "",
+  };
+
+  handleInputChange = (event) => {
+    const newValue = event.target.value;
+    console.log(event.target.name);
+    const newState = {
+      [event.target.name]: event.target.value,
+    };
+
+    this.setState(newState);
+  };
+
+  handleSubmitLogin(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div>
-        <label>
-          User Name
-        </label>
-        <input placeholder='User Name'/>
-
-        <label>
-          Password
-        </label>
-        <input placeholder='Password'/>
+        <div>
+          VALUES SORT CARD APP
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+        </div>
+        <Form onSubmit={this.handleSubmitLogin}>
+          <Form.Field>
+            <label>First Name</label>
+            <input name="userName" onChange={this.handleInputChange} placeholder='User Name'/>
+          </Form.Field>
+          <Form.Field>
+            <label>Last Name</label>
+            <input name="password" onChange={this.handleInputChange} placeholder='Password' />
+          </Form.Field>
+          <Button type="submit">Login</Button>
+        </Form>
       </div>
-    )
+    );
   }
 }
