de3bfa8e8398c243d09a087d53d9b7b9285441b8
PsijicSeht <38445096+Kumar-Krishnan@users.noreply.github.com>
2020-04-24 17:48:28 -0400

    file for creating manual password

---
+++ back_end/manuallyCreatePasswordHash.js
@@ -0,0 +1,18 @@
+// const uuid = require('uuid/v4');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const password = '';
+let hashedPass;
+
+const testFunction = async() =>{
+  hashedPass = await bcrypt.hash(password, saltRounds)
+  console.log("hashed Pass", hashedPass)
+  // console.log("hashedPass", hashedPass)
+  const result = await bcrypt.compare(password, hashedPass)
+  // console.log("RESULT", uuid());
+  const ttl = new Date().getTime() + 7200000;
+  console.log(result)
+  // console.log("TTL", ttl)
+};
+
+testFunction();
