10f8c8cfddd2b4e0a1263cf13be6f19cca0a9f92
PsijicSeht <38445096+Kumar-Krishnan@users.noreply.github.com>
2020-04-24 18:18:30 -0400

    Beginning of new activity page

---
+++ front_end/src/components/NewActivityPage/NewActivityPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class NewActivityPage extends Component {
+  render() {
+    return (
+      <div>
+        New Activity PAGE!!
+      </div>
+    )
+  }
+}
