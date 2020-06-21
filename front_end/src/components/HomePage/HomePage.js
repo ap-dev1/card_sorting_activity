0743d826fa6fd602331c3f3faec0f460458abf8e
PsijicSeht <38445096+Kumar-Krishnan@users.noreply.github.com>
2020-06-21 18:34:28 -0400

    moved to fix multiple auth calls

--- front_end/src/components/HomePage/HomePage.js
+++ front_end/src/components/HomePage/HomePage.js
@@ -1,86 +1,60 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import axios from "axios";
 
-const HomePage =  (props) => {
-  const [authStatus, setAuthStatus] = useState(0);
+export default class HomePage extends Component {
+  state = {
+    authStatus: 0,
+  };
 
+  componentDidMount = async () => {
+    await this.authenticate();
+  };
 
-  const authenticate = async () => {
-    try{
+  authenticate = async () => {
+    try {
       const authenticationResult = await axios({
         method: "post",
         url: "http://127.0.0.1:3001/auth/tokenAuthenticate",
         data: {
-          authorizationName: `${props.match.params.user}`,
-          token: `${props.match.params.sessionId}`,
+          authorizationName: `${this.props.match.params.user}`,
+          token: `${this.props.match.params.sessionId}`,
         },
         crossDomain: true,
       });
-      console.log("result",authenticationResult)
-      setAuthStatus(authenticationResult.data.status)
-    }
-    catch(err){
-      console.log("Err in homepage authentication", JSON.stringify(err))
+      console.log("result", authenticationResult);
+      this.setState({ authStatus: authenticationResult.data.status });
+    } catch (err) {
+      console.log("Err in homepage authentication", JSON.stringify(err));
     }
   };
-  authenticate();
 
+  // Declare the function to be called when clicking the button.
+  // Because we're using "Router", we have access to history, which
+  // is an object with multiple methods. It keeps track of the current
+  // URL and where you've been (browsing wise).
 
+  startActivity = () => {
+    //history is a prop provided by the router, in App.js, render ()
+    this.props.history.push(`${this.props.match.url}/NewActivityPage`);
+  };
 
-// Declare the function to be called when clicking the button.
-// Because we're using "Router", we have access to history, which
-// is an object with multiple methods. It keeps track of the current
-// URL and where you've been (browsing wise).
-
-
-const startActivity = () => {
-
-  //history is a prop provided by the router, in App.js, render ()
-
- 
-
-  props.history.push(`${props.match.url}/NewActivityPage`)}
-
-
-
-
-  if (authStatus === 0){
-    return (
-      <div>Loading HOME PAGE!</div>
-    )
-  }
-
-
-  else if (authStatus == 200){
-    return (
-      <div>
+  render() {
+    if (this.state.authStatus === 0) {
+      return <div>Loading HOME PAGE!</div>;
+    } else if (this.state.authStatus == 200) {
+      return (
+        <div>
           <div>Congrats. Now what? </div>
           <br></br>
-          <button onClick={startActivity}>New Activity</button>
+          <button onClick={() => this.startActivity()}>New Activity</button>
           <br></br>
           <h4>Previous sessions:</h4>
-
-      </div>
-    )
+        </div>
+      );
+    } else if (this.state.authStatus == 440) {
+      return <div>Session Expired! Please re-login</div>;
+    } else if (this.state.authStatus == 401) {
+      return <div>"Get OUT!"</div>;
+    }
   }
-
-
-
-  else if(authStatus == 440){
-    return (
-      <div>Session Expired! Please re-login</div>
-    )
-  }
-
-
-
-  else if(authStatus == 401){
-    return (
-      <div>"Get OUT!"</div>
-    )
-  }
-
-
-};
-
-export default HomePage;
+}
