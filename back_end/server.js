e1d841e598dab8afa2a4f046aa873ea201ac075e
ap-dev1 <64612312+ap-dev1@users.noreply.github.com>
2020-09-03 19:39:06 -0400

    right before moving the back-end

--- back_end/server.js
+++ back_end/server.js
@@ -5,10 +5,10 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
 
-const AWS = require('aws-sdk');
-const { AWS_CREDS } = require('./configs/awsCreds.js');
+// const AWS = require('aws-sdk');
+// const { AWS_CREDS } = require('./configs/awsCreds.js');
 
-AWS.config.update(AWS_CREDS);
+// AWS.config.update(AWS_CREDS);
 
 const {authRouter} = require('./routers/authRouter.js')
 
